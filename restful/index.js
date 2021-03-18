bd18caa67ea849a30cc08504f558272f10343dc4
William Tomé <william.tomenh@gmail.com>
2021-03-17 22:19:00 -0300

    Usar as rotas criadas.

--- restful/index.js
+++ restful/index.js
@@ -1,34 +1,11 @@
-const http = require('http')
+const express = require('express')
+const routesIndex = require('./routes/index')
+const routesUsers = require('./routes/users')
+const app = express()
 
-const server = http.createServer((req, res) => {
-    console.log('URL:',req.url, 'Method',req.method);
+app.use(routesIndex)
+app.use('/users', routesUsers)
 
-    switch (req.url) {
-        case '/':
-            res.statusCode = 200
-            res.setHeader('Content-Type', 'text/html')
-            res.end('<h1>Olá mundo!</h1>')
-            break;
-        
-        case '/users':
-            res.statusCode = 200
-            res.setHeader('Content-Type', 'application/json')
-            res.end(JSON.stringify({
-                users: [
-                    {
-                        name: 'William',
-                        email: 'contato@williamtome.dev',
-                        id: 1
-                    }
-                ]
-            }))
-            break;
-    
-        default:
-            break;
-    }
-})
-
-server.listen(3000, '127.0.0.1', () => {
+app.listen(3000, '127.0.0.1', () => {
     console.log('servidor rodando em http://localhost:3000');
 })
