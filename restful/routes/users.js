87bcadffd5dc13565ebbe57f4716bc53c54e581c
William Tomé <william.tomenh@gmail.com>
2021-03-17 23:49:25 -0300

    Criar um novo usuário.

--- restful/routes/users.js
+++ restful/routes/users.js
@@ -1,3 +1,11 @@
+const db = require('../db')
+
+const create = async(data) => {
+    const dbConn = await db.init('./db.sqlite3')
+    console.log(data)
+    await db.queryWithParams(dbConn, "INSERT INTO users (name, email) VALUES (?, ?);", data)
+}
+
 module.exports = app => {
     app.get('/users', (req, res) => {
         res.statusCode = 200
@@ -11,11 +19,17 @@ module.exports = app => {
         })
     })
     
-    app.get('/users/admin', (req, res) => {
-        res.statusCode = 200
-        res.setHeader('Content-Type', 'application/json')
-        res.json({
-            users: []
-        })
+    app.post('/users', async(req, res) => {
+        
+        // res.json(req.body)
+        await create(req.body)
+            .catch(err => {
+                console.log('Error:',err)
+                res.json({
+                    status: 400,
+                    error: err
+                })
+            })
+
     })
 }
