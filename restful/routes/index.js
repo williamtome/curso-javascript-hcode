5bdb66505b2c5c9093b71d11b1900d109bafd7d8
William Tomé <william.tomenh@gmail.com>
2021-03-17 22:30:40 -0300

    Exportando as rotas.

--- restful/routes/index.js
+++ restful/routes/index.js
@@ -1,10 +1,7 @@
-const express = require('express')
-const routes = express.Router()
-
-routes.get('/', (req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html')
-    res.send('<h1>Olá mundo!</h1>')
-})
-
-module.exports = routes
+module.exports = app => {
+    app.get('/', (req, res) => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/html')
+        res.send('<h1>Olá mundo!</h1>')
+    })
+}
--- restful/routes/users.js
+++ restful/routes/users.js
@@ -1,24 +1,21 @@
-const express = require('express')
-const routes = express.Router()
-
-routes.get('/', (req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json')
-    res.json({
-        users: [{
-            name: 'William',
-            email: 'contato@williamtome.dev',
-            id: 1
-        }]
+module.exports = app => {
+    app.get('/users', (req, res) => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.json({
+            users: [{
+                name: 'William',
+                email: 'contato@williamtome.dev',
+                id: 1
+            }]
+        })
     })
-})
-
-routes.get('/admin', (req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json')
-    res.json({
-        users: []
+    
+    app.get('/users/admin', (req, res) => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.json({
+            users: []
+        })
     })
-})
-
-module.exports = routes
+}
