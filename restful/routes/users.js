b8bdea04af42da454fb59532cc3605e811126e7c
William Tomé <william.tomenh@gmail.com>
2021-03-19 23:17:04 -0300

    Refatorado o código de cadastro e listagem de usuários.

--- restful/routes/users.js
+++ restful/routes/users.js
@@ -5,34 +5,28 @@ const db = new NeDB({
 })
 
 module.exports = app => {
-    app.get('/users', (req, res) => {
 
+    const route = app.route('/users')
+    
+    route.get((req, res) => {
         db.find({}).sort({name:1}).exec((err, users) => {
             if (err) {
-                console.log(`error: ${err}`);
-                res.status(400).json({
-                    error: err
-                })
+                app.utils.error.send(err, req, res)
             } else {
                 res.statusCode = 200
-                res.setHeader('Content-Type', 'application/json')
+                res.setHeader('Content-Type', 'routelication/json')
                 res.json({users})
             }
         })
     })
     
-    app.post('/users', async(req, res) => {
-        
+    route.post((req, res) => {
         db.insert(req.body, (err, user) => {
             if(err){
-                console.log('Error:',err);
-                res.status(400).json({
-                    error: err
-                })
+                app.utils.error.send(err, req, res)
             } else {
                 res.status(200).json(user)
             }
         })
-
     })
 }
