9895c211f38258559854976dd4e2fd9868adcb36
William Tomé <william.tomenh@gmail.com>
2021-03-19 22:57:44 -0300

    Listar todos os usuários.

--- restful/routes/users.js
+++ restful/routes/users.js
@@ -6,14 +6,18 @@ const db = new NeDB({
 
 module.exports = app => {
     app.get('/users', (req, res) => {
-        res.statusCode = 200
-        res.setHeader('Content-Type', 'application/json')
-        res.json({
-            users: [{
-                name: 'William',
-                email: 'contato@williamtome.dev',
-                id: 1
-            }]
+
+        db.find({}).sort({name:1}).exec((err, users) => {
+            if (err) {
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error: err
+                })
+            } else {
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'application/json')
+                res.json({users})
+            }
         })
     })
     
