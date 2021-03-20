8ab8906b3b565de5e8ce8b0d4f3bd0de3ae0c480
William Tomé <william.tomenh@gmail.com>
2021-03-19 23:15:18 -0300

    COndign usando o módulo utils.

--- restful/index.js
+++ restful/index.js
@@ -5,7 +5,7 @@ const app = express()
 app.use(express.urlencoded())
 app.use(express.json())
 
-consign().include('routes').into(app)
+consign().include('routes').include('utils').into(app)
 
 app.listen(3000, '127.0.0.1', () => {
     console.log('servidor rodando em http://localhost:3000');
